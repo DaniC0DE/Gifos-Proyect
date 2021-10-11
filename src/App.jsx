51f85cfa9f3286c03bc93dedd4db48149ffa4de6
Daniel Alvarado <danieltrm1810@gmail.com>
2021-10-11 14:14:13 -0300

    se agregan estados a utilizar y se pasan como props a los componentes para posterior uso

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,55 @@ import { useEffect, useState } from "react";
 import Header from "./Header";
 import Search from "./Search";
 import Gallery from "./Gallery";
+import Loading from "./svg/loadingRings.svg";
+import GifosL from "./svg/gifosLight.svg";
+import GifosD from "./svg/gifosDark.svg";
+import LostLogo from "./svg/lostLogo.svg";
+import searchIcon from "./svg/search.svg";
+import Blank from "./svg/Blank.svg";
+import NotFound from "./svg/notFound.svg";
 
 import "./Styles.css";
 
 function App() {
+  const [search, setSearch] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [info, setInfo] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [noResults, setNoResults] = useState(false);
+
+  let sinResultados = [];
+
   return (
     <div className="App">
-      <Header />
-      <Search />
-      <Gallery />
+      <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <Search
+        search={search}
+        setSearch={setSearch}
+        info={info}
+        setInfo={setInfo}
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        error={error}
+        setError={setError}
+        noResults={noResults}
+        setNoResults={setNoResults}
+        loading={loading}
+        setLoading={setLoading}
+      />
+      <Gallery
+        sinResultados={sinResultados}
+        search={search}
+        setSearch={setSearch}
+        info={info}
+        setInfo={setInfo}
+        loading={loading}
+        setLoading={setLoading}
+        error={error}
+        noResults={noResults}
+      />
     </div>
   );
 }
