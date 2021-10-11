cb7a05808cb06f9ba37a03a83ee783d3bd309362
Daniel Alvarado <danieltrm1810@gmail.com>
2021-10-11 13:46:21 -0300

    change names, and creat 3 components Header, Search and Gallery

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <button></button>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Search.jsx
@@ -0,0 +1,17 @@
+function Search() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h2> Atrévete a perderte entre los mejores GIFs de la Web! </h2>
+      <div>
+        <input type="text" />
+        <button>
+          {" "}
+          <img src="" alt="" />{" "}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
