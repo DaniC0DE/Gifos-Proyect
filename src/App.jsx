7f4905d270d012ea1e7103ba905c7990811e08a0
Daniel Alvarado <danieltrm1810@gmail.com>
2021-10-12 22:36:18 -0300

    Se agrega un manejador mododark a Header, se config img y button respectivo para ambos modos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Header from "./Header";
 import Search from "./Search";
 import Gallery from "./Gallery";
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,24 @@
-function Header() {
+import GifosL from "./svg/gifosLight.svg";
+import GifosD from "./svg/gifosDark.svg";
+import "./styles.css";
+
+function Header(props) {
+  const handleDark = (props) => {
+    props.setIsDarkMode(!props.isDarkMode);
+  };
+
   return (
-    <div>
-      <img src="" alt="" />
-      <button></button>
+    <div className="HeaderContainer">
+      <img
+        className="Header-title"
+        width="250px"
+        src={props.isDarkMode ? GifosD : GifosL}
+        alt="gifosLogo"
+      />
+      <button onClick={handleDark} className="Header-button">
+        {" "}
+        MODO {props.isDarkMode ? "LIGHT" : "DARK"}{" "}
+      </button>
     </div>
   );
 }
